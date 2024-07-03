4bd669be358f3cfde66ac64a0a74e84148a394c2
Davi Cheli Miquelim <davi.miquelim@bolttech.io>
2024-07-03 20:24:57 -0300

    wip: barebones of the lexer

---
+++ lox.go
@@ -0,0 +1,70 @@
+package lox
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+)
+
+var hadError = false
+
+func main(args string[]) {
+    if len(args) > 1 {
+        fmt.Println("Usage: glox [script]") 
+        os.Exit(64)
+    } else if len(args) == 1 {
+        runFile(args[0])
+    } else {
+        runPrompt()
+    }
+}
+
+func runFile(path string) {
+    if hadError == true {
+        os.Exit(65)
+    }
+
+    data, err := os.ReadFile(path)   
+
+    if err != nil {
+        panic(err)
+    }
+
+    run(string(data))
+}
+
+func runPrompt() {
+    for {
+        scanner := bufio.NewScanner(os.Stdin)
+        fmt.Println("> ")
+
+        didScan := scanner.Scan()
+
+        if didScan == false {
+            break
+        }
+
+        run(scanner.Text())
+        hadError = false
+    }
+}
+
+func run(source string) {
+    scanner := bufio.NewScanner(source)
+    scanner := bufio.Split(bufio.ScanWords)
+
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+}
+
+func error(line int, message string) {
+    report(line, message)
+}
+
+func report(line int, where, message string) {
+    err := fmt.Errorf("[line %d] Error %s: %s")
+    hadError = true
+    fmt.Println(err)
+}
+
