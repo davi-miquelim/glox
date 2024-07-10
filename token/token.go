8333399928db0b070d0e36a3d46f812700618ed9
davi-miquelim <davichelimiquelim@gmail.com>
2024-07-10 17:33:33 -0300

    feat: make token struct properties public

--- token/token.go
+++ token/token.go
@@ -5,24 +5,24 @@ import (
 )
 
 type Token struct {
-    tokenType int
-    line int
-    lexeme  string
-    literal interface{}
+    TokenType int
+    Line int
+    Lexeme  string
+    Literal interface{}
 }
 
 func NewToken(tokenType int, lexeme string, literal *interface{}, line int) *Token {
     if literal == nil {
-        t := Token{lexeme: lexeme, tokenType: tokenType, line: line}
+        t := Token{Lexeme: lexeme, TokenType: tokenType, Line: line}
         return &t
     }
 
-    t := Token{lexeme: lexeme, literal: *literal, tokenType: tokenType, line: line}
+    t := Token{Lexeme: lexeme, Literal: *literal, TokenType: tokenType, Line: line}
     return &t
 }
 
 func (t *Token) ToString() string {
-    str := fmt.Sprintf("%d %s %s", t.tokenType, t.lexeme, t.literal)
+    str := fmt.Sprintf("%d %s %s", t.TokenType, t.Lexeme, t.Literal)
     return str
 }
 
