4fb7392299431d0f68d1cea3201be63fc920fdf3
davi-miquelim <davichelimiquelim@gmail.com>
2024-07-12 16:41:53 -0300

    WIP: ast printer

--- astprinter/astprinter.go
+++ astprinter/astprinter.go
@@ -1,7 +1,42 @@
 package astprinter
 
-import "glox/ast"
+import (
+	"fmt"
+	"glox/ast"
+	"strings"
+)
 
-func visitBinary(expr *ast.Binary) {
+func visitBinary(expr ast.Binary) {
+    return parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right)
+}
+
+func visitGroupingExpr(expr ast.Grouping) {
+    return parenthesize("group", expr.Expression)
+}
+
+func visitLiteralExpr(expr ast.Literal) string {
+    if expr.Value == nil {
+        return "nil"
+    }
+
+    strVal := fmt.Sprintf("%v", expr.Value)
+    return strVal
+}
+
+func visitUnaryExpr(expr ast.Unary) {
+    return parenthesize(expr.Operator.Lexeme, expr.Right)
+}
+
+func parenthesize(name string, exprs ...ast.Visitor{}) {
+    var builder strings.Builder
+
+    builder.WriteByte('(')
+    builder.WriteString(name)
+    for _, expr := range exprs {
+        builder.WriteByte(' ')
+        builder.WriteString(expr.accept())
+    }
+
+    return builder.String()
 }
 
