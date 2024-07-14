a74c50686e83a507f96823dbff168845c328b3a9
davi-miquelim <davichelimiquelim@gmail.com>
2024-07-14 12:16:17 -0300

    style: format files

--- ast/ast.go
+++ ast/ast.go
@@ -1,49 +1,48 @@
 package ast
 
 import (
-    "glox/token"
-    "errors"
+	"errors"
+	"glox/token"
 )
 
 type Visitor interface {
-    VisitForGrouping(*Grouping) interface{}
-    VisitForLiteral(*Literal) interface{}
-    VisitForBinary(*Binary) interface{}
-    VisitForUnary(*Unary) interface{} 
+	VisitForGrouping(*Grouping) interface{}
+	VisitForLiteral(*Literal) interface{}
+	VisitForBinary(*Binary) interface{}
+	VisitForUnary(*Unary) interface{}
 }
 
 type Expression struct {
-    *Literal
-    *Grouping
-    *Binary
-    *Unary
+	*Literal
+	*Grouping
+	*Binary
+	*Unary
 }
 
 func (obj *Expression) Accept(v Visitor) (interface{}, error) {
-    if obj.Literal != nil {
-        return v.VisitForLiteral(obj.Literal), nil
-    } else if obj.Grouping != nil {
-        return v.VisitForGrouping(obj.Grouping), nil
-    } else if obj.Binary != nil {
-        return v.VisitForBinary(obj.Binary), nil
-    } else if obj.Unary != nil {
-        return v.VisitForUnary(obj.Unary), nil
-    } else {
-        return nil, errors.New("nil expression")
-    }
+	if obj.Literal != nil {
+		return v.VisitForLiteral(obj.Literal), nil
+	} else if obj.Grouping != nil {
+		return v.VisitForGrouping(obj.Grouping), nil
+	} else if obj.Binary != nil {
+		return v.VisitForBinary(obj.Binary), nil
+	} else if obj.Unary != nil {
+		return v.VisitForUnary(obj.Unary), nil
+	} else {
+		return nil, errors.New("nil expression")
+	}
 }
-
 
 type Literal struct {
 	Value interface{}
 }
 
 func (obj *Literal) Accept(v Visitor) {
-    if obj == nil {
-        panic("nil Literal")
-    }
+	if obj == nil {
+		panic("nil Literal")
+	}
 
-    v.VisitForLiteral(obj)
+	v.VisitForLiteral(obj)
 }
 
 type Grouping struct {
@@ -51,36 +50,36 @@ type Grouping struct {
 }
 
 func (obj *Grouping) Accept(v Visitor) {
-    if obj == nil {
-        panic("nil Grouping")
-    }
+	if obj == nil {
+		panic("nil Grouping")
+	}
 
-    v.VisitForGrouping(obj)
+	v.VisitForGrouping(obj)
 }
 
 type Binary struct {
-    Left     Expression
+	Left     Expression
 	Right    Expression
 	Operator token.Token
 }
 
 func (obj *Binary) Accept(v Visitor) {
-    if obj == nil {
-        panic("nil Binary")
-    }
+	if obj == nil {
+		panic("nil Binary")
+	}
 
-    v.VisitForBinary(obj)
+	v.VisitForBinary(obj)
 }
 
 type Unary struct {
-	Right     Expression
+	Right    Expression
 	Operator token.Token
 }
 
 func (obj *Unary) Accept(v Visitor) {
-    if obj == nil {
-        panic("nil Unary")
-    }
+	if obj == nil {
+		panic("nil Unary")
+	}
 
-    v.VisitForUnary(obj)
+	v.VisitForUnary(obj)
 }
--- astprinter/astprinter.go
+++ astprinter/astprinter.go
@@ -6,46 +6,45 @@ import (
 	"strings"
 )
 
-type AstPrinter struct {}
+type AstPrinter struct{}
 
 func (ast *AstPrinter) VisitForBinary(expr *ast.Binary) interface{} {
-    return ast.parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right)
+	return ast.parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right)
 }
 
 func (ast *AstPrinter) VisitForGrouping(expr *ast.Grouping) interface{} {
-    return ast.parenthesize("group", expr.Expression)
+	return ast.parenthesize("group", expr.Expression)
 }
 
 func (ast *AstPrinter) VisitForLiteral(expr *ast.Literal) interface{} {
-    if expr.Value == nil {
-        return "nil"
-    }
+	if expr.Value == nil {
+		return "nil"
+	}
 
-    strVal := fmt.Sprintf("%v", expr.Value)
-    return strVal
+	strVal := fmt.Sprintf("%v", expr.Value)
+	return strVal
 }
 
 func (ast *AstPrinter) VisitForUnary(expr *ast.Unary) interface{} {
-    return ast.parenthesize(expr.Operator.Lexeme, expr.Right)
+	return ast.parenthesize(expr.Operator.Lexeme, expr.Right)
 }
 
 func (ast *AstPrinter) parenthesize(name string, exprs ...ast.Expression) interface{} {
-    var builder strings.Builder
+	var builder strings.Builder
 
-    builder.WriteByte('(')
-    builder.WriteString(name)
-    for _, expr := range exprs {
-        builder.WriteByte(' ')
-        val, err := expr.Accept(ast)
+	builder.WriteByte('(')
+	builder.WriteString(name)
+	for _, expr := range exprs {
+		builder.WriteByte(' ')
+		val, err := expr.Accept(ast)
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        strVal := fmt.Sprintf("%v", val)
-        builder.WriteString(strVal)
-    }
+		strVal := fmt.Sprintf("%v", val)
+		builder.WriteString(strVal)
+	}
 
-    return builder.String()
+	return builder.String()
 }
-
