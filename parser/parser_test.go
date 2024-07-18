3547b3617ba59e7e36c75b7b44ac4a5c79c69d6b
davi-miquelim <davichelimiquelim@gmail.com>
2024-07-18 14:59:47 -0300

    feat: add valid test case

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,11 +7,11 @@ import (
 	"testing"
 )
 
-
 func TestParser(t *testing.T) {
-    tokens := []token.Token{*token.NewToken(token.EqualEqual, "==", nil, 0)}
-    loxParser := parser.NewParser(tokens)
-    res := loxParser.Parse()
-    fmt.Println(res)
-
+	var iNum interface{} = 1
+    eof := token.NewToken(token.Eof, "", nil, 0)
+	tokens := []token.Token{*token.NewToken(token.Number, "1", &iNum, 0), *token.NewToken(token.EqualEqual, "==", nil, 0), *token.NewToken(token.Number, "1", &iNum, 0), *eof}
+	loxParser := parser.NewParser(tokens)
+	res := loxParser.Parse()
+	fmt.Println(res)
 }
