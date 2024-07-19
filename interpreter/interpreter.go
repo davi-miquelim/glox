cff470c6c332fe802894c9fc65e11c02fe20c5f5
davi-miquelim <davichelimiquelim@gmail.com>
2024-07-19 16:59:09 -0300

    wip(interpreter): add VisitForBinary

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"glox/ast"
 	"glox/token"
+	"reflect"
 	"strconv"
+	"strings"
 )
 
 type interpreter struct {}
@@ -45,6 +47,59 @@ func (i *interpreter) isTruthy(val interface{}) bool {
     }
 }
 
+func (i *interpreter) VisitForGrouping(expr *ast.Grouping) interface{} {
+    return i.evaluate(expr.Expression)
+}
+
+func (i *interpreter) VisitForBinary(expr *ast.Binary) interface{} {
+    left := i.evaluate(expr.Left.Expression)
+    right := i.evaluate(expr.Right.Expression)
+
+    l, lErr := i.convertToFloat64(left)
+    r, rErr := i.convertToFloat64(right)
+
+    hasConvErr := lErr != nil && rErr != nil
+    if hasConvErr && expr.Operator.TokenType == token.Plus {
+        strL := fmt.Sprintf("%v", left)
+        strR := fmt.Sprintf("%v", right)
+
+        var builder strings.Builder
+        builder.WriteString(strL)
+        builder.WriteString(strR)
+
+        return builder.String()
+    }
+
+    if hasConvErr && expr.Operator.TokenType != token.Plus {
+        // add error handling afterwards
+    }
+
+    switch expr.Operator.TokenType {
+    case token.Minus:
+        return l - r
+    case token.Slash:
+        return l / r
+    case token.Star:
+        return l * r
+    case token.Plus:
+        return l + r
+    case token.Greater:
+        return l > r
+    case token.GreaterEqual:
+        return l >= r
+    case token.Less:
+        return l < r
+    case token.LessEqual:
+        return l <= r
+    case token.BangEqual:
+        return !(l == r)
+    case token.EqualEqual:
+        return  l == r
+    }
+
+    return nil
+}
+
 func (i *interpreter) convertToFloat64(val interface{}) (float64, error) {
     switch v := val.(type) {
     case int:
@@ -62,10 +117,6 @@ func (i *interpreter) convertToFloat64(val interface{}) (float64, error) {
     default:
         return -1, fmt.Errorf("Can't convert to float64")
     }
-}
-
-func (i *interpreter) VisirForGrouping(expr *ast.Grouping) interface{} {
-    return i.evaluate(expr.Expression)
 }
 
 func (i *interpreter) evaluate(expr ast.Expression) interface{} {
